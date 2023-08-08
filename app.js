5251c6bff1881a49f5e18354359294f751372d7b
krgarav <krgarav@gmail.com>
2023-08-08 20:45:46 +0530

    updated some code

--- app.js
+++ app.js
@@ -15,24 +15,34 @@ const Forgotpassword = require("./Models/forgotpassword");
 const Expensedownload = require("./Models/downloadexpense");
 const dotenv = require('dotenv');
 const path = require("path");
-const builtPath=path.join(__dirname,"../frontend_expense_tracker/dist");
+const builtPath = path.join(__dirname, "../ExpenseTrackerFrontend/dist");
 
 dotenv.config();
 
-const accessLogs = fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'});
+const accessLogs = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
 app.use(bodyParser.json({ limit: '1mb' }))
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: '*',  // Allow requests from this origin
+  methods: ['OPTIONS', 'POST', 'GET', 'DELETE'], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorisation'],  // Allow these headers
+}));
 
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Origin-Agent-Cluster', 'true');
+  next();
+});
 app.use("/user/", authRoutes);
 app.use("/expense/", expenseRoutes);
 app.use("/purchase/", purchaseRoutes);
 app.use("/password/", passwordRoutes);
 app.use(express.static(builtPath))
-app.use((req,res)=>{
-  res.sendFile(path.join(__dirname,"../frontend_expense_tracker/dist/index.html"));
+app.use((req, res) => {
+
+  res.sendFile(path.join(__dirname, "../ExpenseTrackerFrontend/dist/index.html"));
 })
 
 User.hasMany(Expense);
